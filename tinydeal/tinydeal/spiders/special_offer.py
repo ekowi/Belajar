f58674fd7bb5d41ad2af40d959d794b3d94fc8af
Eko <greget103@gmail.com>
2022-07-03 08:24:20 +0700

    handle page ignation

--- tinydeal/tinydeal/spiders/special_offer.py
+++ tinydeal/tinydeal/spiders/special_offer.py
@@ -10,6 +10,12 @@ class SpecialOfferSpider(scrapy.Spider):
         for product in response.xpath("//ul[@class='productlisting-ul']/div/li"):
             yield {
                 'tittle' : product.xpath(".//a[@class='p_box_title']/text()").get(),
+                'url' : response.urljoin(product.xpath(".//a[@class='p_box_title']/@href").get()),
                 'discount_price' : product.xpath(".//div[@class='p_box_price']/span[1]/text()").get(),
                 'real_price': product.xpath(".//div[@class='p_box_price']/span[2]/text()").get()
             }
+
+        next_page = response.xpath("//a[@class='nextPage']").get()
+
+        if next_page:
+            yield scrapy.Request(url= next_page, callback=self.parse)
